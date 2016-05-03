eb5498b24ac36c0d783a023f06f7046da0724cd5
IoannisChadjiminas <chadjiminas.ioannis@gmail.com>
2016-05-03 09:16:15 +0100

    added more

--- api/static/api/assets/js/NewsList.js
+++ api/static/api/assets/js/NewsList.js
@@ -4,6 +4,14 @@ var NewsItem = require('./StoryItem');
 var React = require('react');
 
 var NewsList = React.createClass({
+  getMore: function () {
+    return (
+      <div className="newsList-more">
+        <a className="newsList-moreLink" href="">More</a>
+      </div>
+    );
+  },
+
   render: function () {
     return (
       <div className="newsList">
@@ -12,6 +20,7 @@ var NewsList = React.createClass({
           {_(this.props.items).map(function (item, index) {
             return <NewsItem key={item.id} item={item} rank={index + 1}/>;
           }.bind(this)).value()}
+          {this.getMore()}
         </div>
       </div>
     );
