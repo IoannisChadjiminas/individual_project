c6a709140b62c04de2cf9469974a149977ff7a75
IoannisChadjiminas <chadjiminas.ioannis@gmail.com>
2016-05-03 03:12:53 +0100

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # SECRET_KEY = os.environ["SECRET_KEY"]
 SECRET_KEY = "245r22q435135542332"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # Application definition
