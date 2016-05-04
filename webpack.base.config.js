76bc660aeccf01c8cdb75b0f3fa2dce657893f7f
IoannisChadjiminas <chadjiminas.ioannis@gmail.com>
2016-05-04 21:45:08 +0100

    Basic UI of the website

---
+++ webpack.base.config.js
@@ -0,0 +1,31 @@
+var path = require('path')
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = {
+  context: __dirname,
+
+  entry: './api/static/api/assets/js/NewsListTest.js', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+
+  output: {
+      path: path.resolve('./api/static/api/assets/bundles/'),
+      filename: "[name]-[hash].js",
+  },
+
+  plugins: [
+         new webpack.ProvidePlugin({ 
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery' 
+        })
+  ], //add all common plugins here
+
+  module: {
+    loaders: [], //add all common loaders here
+  },
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'bower_components'],
+    extensions: ['', '.js', '.jsx']
+  },
+}
