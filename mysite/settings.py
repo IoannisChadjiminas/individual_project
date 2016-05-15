467b145c3ad66250bd0f9ca6e9cba64e4ff59c0d
IoannisChadjiminas <chadjiminas.ioannis@gmail.com>
2016-05-15 18:55:27 +0100

    Basic UI navBar + basic articleList

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,10 +26,11 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: change this before deploying to production!
-# SECRET_KEY = os.environ["SECRET_KEY"]
-SECRET_KEY = "245r22q435135542332"
+SECRET_KEY = os.environ["SECRET_KEY"]
+#SECRET_KEY = "245r22q435135542332"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 
 # Application definition
