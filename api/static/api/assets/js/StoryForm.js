e1896863cbeef14922182ce4cdcfa54bf81fa140
IoannisChadjiminas <chadjiminas.ioannis@gmail.com>
2016-05-30 13:42:13 +0100

    url validation

--- api/static/api/assets/js/StoryForm.js
+++ api/static/api/assets/js/StoryForm.js
@@ -33,11 +33,46 @@ class StoryForm extends React.Component {
       var url = this.state.url.trim();
 
       //var regex = RegExp("^((((https?|ftps?|gopher|telnet|nntp)://)|(mailto:|news:))(%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@&=+$,A-Za-z0-9])+)([).!';/?:,][[:blank:]])?$")
-      var regex = RegExp("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i")
-      console.log(regex.test(url))
+      var re_weburl = new RegExp(
+            "^" +
+              // protocol identifier
+              "(?:(?:https?|ftp)://)" +
+              // user:pass authentication
+              "(?:\\S+(?::\\S*)?@)?" +
+              "(?:" +
+                // IP address exclusion
+                // private & local networks
+                "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
+                "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
+                "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+                // IP address dotted notation octets
+                // excludes loopback network 0.0.0.0
+                // excludes reserved space >= 224.0.0.0
+                // excludes network & broacast addresses
+                // (first & last IP address of each class)
+                "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+                "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+                "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+              "|" +
+                // host name
+                "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" +
+                // domain name
+                "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" +
+                // TLD identifier
+                "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
+                // TLD may end with dot
+                "\\.?" +
+              ")" +
+              // port number
+              "(?::\\d{2,5})?" +
+              // resource path
+              "(?:[/?#]\\S*)?" +
+            "$", "i"
+          );
+      console.log(re_weburl.test(url))
       
 
-      if (!title || !url || !regex.test(url)) {
+      if (!title || !url || !re_weburl.test(url)) {
         this.setState({url_format:true})
         return;
       }
