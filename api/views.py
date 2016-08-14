89b0c75f7e34de4cfd7a91669637b05ddacc7460
IoannisChadjiminas <chadjiminas.ioannis@gmail.com>
2016-08-14 20:48:59 +0100

    ranking_system

--- api/views.py
+++ api/views.py
@@ -135,11 +135,11 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
                     serializer.save(score=int(self.request.data['score']), score_sad=int(self.request.data['score_sad']))
         else:
             if int(self.request.data['emotion']) == 2:
-                serializer.save(score=int(self.request.data['score']), score_happy=int(self.request.data['score_happy']) +1)
+                serializer.save(score=int(self.request.data['score']), score_happy=post.score_happy +1)
             elif int(self.request.data['emotion']) == 3:
-                serializer.save(score=int(self.request.data['score']), score_wow=int(self.request.data['score_wow']) +1)
+                serializer.save(score=int(self.request.data['score']), score_wow=post.score_wow +1)
             elif int(self.request.data['emotion']) == 4:
-                serializer.save(score=int(self.request.data['score']), score_sad=int(self.request.data['score_sad']) +1)
+                serializer.save(score=int(self.request.data['score']), score_sad=post.score_sad +1)
 
 
     '''
