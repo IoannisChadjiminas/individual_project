e66642170df5b2d22f84898ebc734045e1548533
IoannisChadjiminas <chadjiminas.ioannis@gmail.com>
2016-08-14 20:10:46 +0100

    ranking_system

--- api/views.py
+++ api/views.py
@@ -113,11 +113,11 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
         if Voter.objects.filter(post=post.id, user=self.request.user).exists():
             voter_relation = Voter.objects.get(post=post.id, user=self.request.user)
             if (voter_relation.emotion == 2):
-                serializer.save(score=int(self.request.data['score']) -1, score_happy=int(self.request.data['score_happy']) -1)
+                serializer.save(score=int(self.request.data['score']), score_happy=int(self.request.data['score_happy']) -1)
             elif (voter_relation.emotion == 3):
-                serializer.save(score=int(self.request.data['score']) -1, score_wow=int(self.request.data['score_wow']) -1)
+                serializer.save(score=int(self.request.data['score']), score_wow=int(self.request.data['score_wow']) -1)
             elif (voter_relation.emotion == 4):
-                serializer.save(score=int(self.request.data['score']) -1, score_sad=int(self.request.data['score_sad']) -1)
+                serializer.save(score=int(self.request.data['score']), score_sad=int(self.request.data['score_sad']) -1)
         else:
             if int(self.request.data['emotion']) == 2:
                 serializer.save(score=int(self.request.data['score']) +1, score_happy=int(self.request.data['score_happy']) +1)
