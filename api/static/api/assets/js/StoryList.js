6116d2e24582a334b15a05630ef8dda644e06551
IoannisChadjiminas <chadjiminas.ioannis@gmail.com>
2016-08-16 14:58:37 +0100

    ranking system

--- api/static/api/assets/js/StoryList.js
+++ api/static/api/assets/js/StoryList.js
@@ -262,7 +262,7 @@ export class StoryItem extends React.Component {
           <div  className="storyItem-storyItems">
                   <br />                  
                   <hr />
-                  <div> {this.props.item} </div>
+                  <div> {this.props.id} </div>
                   <div className = "storyTitle"> <StoryWrapTitle  title={this.props.title} score={this.props.score} score_lol={this.props.score_lol} score_wow={this.props.score_wow} time_difference={this.props.time_difference}  
                   score_happy={this.props.score_happy} score_angry={this.props.score_angry} score_sad={this.props.score_sad} url={this.props.url} owner={this.props.owner}/> </div>
                   <p className='snippet'> {this.props.snippet} </p>
