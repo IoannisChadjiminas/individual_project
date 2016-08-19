b4320e1a299ef11948ca5dc9e6d54a3292f2615d
IoannisChadjiminas <chadjiminas.ioannis@gmail.com>
2016-08-19 18:43:49 +0100

    last version

--- mysite/settings.py
+++ mysite/settings.py
@@ -174,6 +174,12 @@ if not DEBUG:
 # }
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
+REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
