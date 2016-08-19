864d9e27b819a3881a52e2db5d7cf6de88f5f813
IoannisChadjiminas <chadjiminas.ioannis@gmail.com>
2016-08-19 15:29:49 +0100

    user_uploaded_story

--- api/static/api/assets/js/StoryForm.js
+++ api/static/api/assets/js/StoryForm.js
@@ -169,8 +169,8 @@ class StoryForm extends React.Component {
             </FormGroup>
             <FormGroup>
               {this.state.url_format ? <p> <p style = {wrongDetail} > <b> There was a problem! </b> </p> <p> - <b> The story has been already uploaded! </b> </p> <p> - <b> Or, You have exceeded the limit of five stories per day </b> </p> </p>: <span />}
-              {this.props.many_request_error && !this.state.url_format ? <p> <p style={wrongUrl} > <b> One field seems to be incorrect! </b> </p> </p>} 
-              {this.props.story_submitted && !this.props.url_format && !this.props.many_request_error ? <p> <p style={storySubmitted}> <b> Your story has been submitted! </b> </p> <p> <b> Once has been approved, it will be shown on the website's interface! </b> </p> </p> : <span /> }
+              {/*{this.props.many_request_error && !this.state.url_format ? <p> <p style={wrongUrl} > <b> One field seems to be incorrect! </b> </p> </p>} 
+              {this.props.story_submitted && !this.props.url_format && !this.props.many_request_error ? <p> <p style={storySubmitted}> <b> Your story has been submitted! </b> </p> <p> <b> Once has been approved, it will be shown on the website's interface! </b> </p> </p> : <span /> }*/}
             </FormGroup>
           </Form>
         )
